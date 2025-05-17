21e713c5b6e52469cb0c7a09e007fce8318a8631
arun24hrs <2017arun.rana@gmail.com>
2025-05-17 15:21:29 +0530

    added middleware and routes

--- todo-backend/Routes/userRoute.js
+++ todo-backend/Routes/userRoute.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const bcrypt = require("bcrypt");
 const UserModel = require("../Model/userModel");
 require("dotenv").config();
+const jwt = require("jsonwebtoken")
 
 const userRouter = Router();
 
@@ -9,12 +10,12 @@ userRouter.post("/register", async (req, res) => {
   const { email, password } = req.body;
   if (email !== undefined) {
     const userExist = await UserModel.findOne({ email });
-    console.log(userExist, "user")
+    console.log(userExist, "user");
     if (!userExist) {
       bcrypt.hash(password, 5, async function (err, hash) {
-        console.log(hash, "hash")
+        console.log(hash, "hash");
         if (hash) {
-          const user = UserModel({ email, password: hash });
+          const user = await UserModel({ email, password: hash });
           await user.save();
           res.status(201).send({ message: "User Registered Successfully!" });
         } else {
@@ -24,6 +25,28 @@ userRouter.post("/register", async (req, res) => {
     } else {
       res.status(200).send({ message: "User already registered!" });
     }
+  }
+});
+
+userRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body; //arun.k@meritto.com wqerwetl234
+  try {
+    const userExist = await UserModel.findOne({ email });
+    if (userExist) {
+        bcrypt.compare(password, userExist.password, function (err, result) {
+      if(result){
+        const token = jwt.sign({userID: userExist._id, userEmail: userExist.email}, process.env.SECRET_KEY);
+        res.status(201).send({msg: "Login Successful!", "token": `Bearer ${token}`})
+      } else {
+         res.status(200).send({msg: "Wrong Credentials!"});
+      }
+    });
+    } else {
+        res.status(200).send({msg: "No usesr found!"});
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({msg: "Something went wrong!"})
   }
 });
 
--- todo-backend/index.js
+++ todo-backend/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { connection } = require("./connection");
 const userRouter = require("./Routes/userRoute");
-const cors = require("cors")
+const cors = require("cors");
+const auth = require("./Middlewares/auth.middleware");
+const taskRouter = require("./Routes/tasksRoute");
 
 require("dotenv").config();
 const app = express();
@@ -9,6 +11,8 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 app.use('/users', userRouter);
+app.use(auth);
+app.use('/tasks', taskRouter);
 
 
 app.listen(process.env.PORT, async()=>{
